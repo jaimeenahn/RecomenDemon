01f9a2a575564dabcbaf3d8a9b87a119f802440a
dongkyul <dongkyul@buffalo.edu>
2018-03-26 21:27:29 -0400

    commit scene and game

--- game.js
+++ game.js
@@ -3,5 +3,12 @@ class Game {
         this.images = null
         this.canvas = canvas
         this.context = canvas.getContext('2d')
-  }
-    
+        this.sceneFactory = this.sceneFactory || new SceneFactory(this)
+    }
+
+    init (){
+
+        let scene = this.sceneFactory.getSceneTitleInstance()
+        scene.init()
+    }
+}
