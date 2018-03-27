506cc23e0da8b1d8e5da796898433ec406b39f36
dongkyul <dongkyul@buffalo.edu>
2018-03-26 22:16:44 -0400

    commit scene and game

--- game.js
+++ game.js
@@ -5,9 +5,9 @@ class Game {
         this.context = canvas.getContext('2d')
         this.sceneFactory = this.sceneFactory || new SceneFactory(this)
     }
-
+    //intialize the game
     init (){
-
+    //take game as parameter to the scene
         let scene = this.sceneFactory.getSceneTitleInstance()
         scene.init()
     }
