ca653676dc009e2280ebb055942e57de9807e691
ubbh <bohanghu@buffalo.edu>
2018-05-01 23:11:52 -0400

    scene/scene_editor.js

--- scene/scene_editor.js
+++ scene/scene_editor.js
@@ -73,7 +73,7 @@ class SceneEditor extends Scene{
                     // man
                     this.man.x = i
                     this.man.y = j
-                    this.drawItem(j, i, 'currentblock')
+                    //this.drawItem(j, i, 'currentblock')
                     this.drawItem(j, i, this.man.direction)
                 }
                 if (map[i][j] == MAP_CODE.boxedBull){
